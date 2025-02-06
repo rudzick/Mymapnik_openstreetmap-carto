d2aa57b43e3543370af2dc8d2eeb29a9c4303722
rudzick <oliver@rudzick.de>
2025-02-06 02:46:21 +0100

    funcions.sql hinzugefügt

--- Server_Update/create_views_for_bbox_server.sh
+++ Server_Update/create_views_for_bbox_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 # als user postgres
-psql -d gis -a -f /home/renderaccount/Mymapnik_openstreetmap-carto/Server_Update/views_for_bbox_server.sql
-psql -d gis -a -f /home/renderaccount/Mymapnik_openstreetmap-carto/indexes.sql
+psql -d gis -a -f /home/renderaccount/Mymapnik_openstreetmap-carto/functions.sql
+psql -d gis -a -f /home/renderaccount/Mymapnik_openstreetmap-carto/indexes.sqlpsql -d gis -a -f /home/renderaccount/Mymapnik_openstreetmap-carto/Server_Update/views_for_bbox_server.sql
+
 
