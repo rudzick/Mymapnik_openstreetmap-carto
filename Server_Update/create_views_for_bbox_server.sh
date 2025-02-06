596c64e4584b6eb94cc09149282f962553fb7fc4
rudzick <oliver@rudzick.de>
2025-02-06 02:40:30 +0100

    index.sql hinzugefügt

--- Server_Update/create_views_for_bbox_server.sh
+++ Server_Update/create_views_for_bbox_server.sh
@@ -2,3 +2,5 @@
 set -euo pipefail
 # als user postgres
 psql -d gis -a -f /home/renderaccount/Mymapnik_openstreetmap-carto/Server_Update/views_for_bbox_server.sql
+psql -d gis -a -f /home/renderaccount/Mymapnik_openstreetmap-carto/indexes.sql
+
